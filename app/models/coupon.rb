e8dda571cf68587fd1c688b93ae1f6d331818e14
Joe Chirchirillo <cchirchirillo3@gmail.com>
2024-09-20 15:58:22 -0600

    Feat: validations pt1

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -1,4 +1,6 @@
 class Coupon < ApplicationRecord
   belongs_to :merchant
-  has_many :coupons
+  has_many :invoices
+
+  validates :code, uniqueness: {message: "This coupon code already exists"}
 end
