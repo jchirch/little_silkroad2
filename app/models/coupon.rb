0ceef9c54b1cba932827feff453c1468faf985aa
Joe Chirchirillo <cchirchirillo3@gmail.com>
2024-09-21 15:05:50 -0600

    Feat: validations and tests

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -2,5 +2,9 @@ class Coupon < ApplicationRecord
   belongs_to :merchant
   has_many :invoices
 
+  validates :name, presence: { message: "Name is required"}
+  validates :code, presence: { message: "Code is required"}
   validates :code, uniqueness: {message: "This coupon code already exists"}
+  validates :discount_type, presence: {message: "Must be 'dollar' or 'percent'"}
+  validates :active, presence: {message: "Status is required"}
 end
--- spec/models/coupon_spec.rb
+++ spec/models/coupon_spec.rb
@@ -7,6 +7,10 @@ RSpec.describe Coupon, type: :model do
   end
 
   describe 'validations' do
+    it {should validate_presence_of(:name).with_message("Name is required")}
+    it {should validate_presence_of(:code).with_message("Code is required")}
     it {should validate_uniqueness_of(:code).with_message("This coupon code already exists")}
+    it {should validate_presence_of(:discount_type).with_message("Must be 'dollar' or 'percent'")}
+    it {should validate_presence_of(:active).with_message("Status is required")}
   end
 end
