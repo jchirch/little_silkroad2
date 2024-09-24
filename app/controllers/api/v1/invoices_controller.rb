3f8d1763b55144464fdc845ce570799e58fe0763
Joe Chirchirillo <cchirchirillo3@gmail.com>
2024-09-23 19:42:03 -0600

    Cleanup

--- app/controllers/api/v1/invoices_controller.rb
+++ app/controllers/api/v1/invoices_controller.rb
@@ -1,13 +1,7 @@
 class Api::V1::InvoicesController < ApplicationController
   def index
     merchant = Merchant.find(params[:merchant_id])
-    invoices = merchant.invoices
-    render json: InvoiceSerializer.new(invoices)
+    render json: InvoiceSerializer.new(merchant.invoices)
   end
-
-  # def index
-  #   merchant = Merchant.find(params[:merchant_id])
-  #   render json: InvoiceSerializer.new(merchant.invoices)
-  # end
 
 end
